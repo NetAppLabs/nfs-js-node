353ed2c4b3b215c599688c6d145f01a7c2deb53b
fridvin-netapp <Fridvin.Oddbjornsson@netapp.com>
2025-05-07 10:27:19 +0000

    Added git action to publish npm package @netapplabs/nfs-js-node

--- deps.sh
+++ deps.sh
@@ -15,8 +15,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-
-set -e
+set -ex
+TARGET_TRIPLE=$1
+TARGET_TRIPLE_FOR_CC=$2
 
 if ! command -v git 2>&1 >/dev/null ; then
     if command -v brew 2>&1 >/dev/null ; then
@@ -44,6 +45,33 @@ if ! command -v automake 2>&1 >/dev/null ; then
         sudo apt-get install -y automake
     else
         echo "please install automake"
+    fi
+fi
+
+if ! command -v add-apt-repository 2>&1 >/dev/null ; then
+    if command -v apt-get 2>&1 >/dev/null ; then
+        sudo apt-get update
+        sudo apt-get -y install software-properties-common
+    fi
+fi
+
+if command -v lsb_release 2>&1 >/dev/null ; then
+    lsb_rel_version=`lsb_release -c | grep '^Codename:' | awk -F ' ' '{print $2}'`
+    if [ "${lsb_rel_version}" == "focal" ]; then
+        # install backported autoconf 2.71 backported for ubuntu 20.04 / focal
+        sudo add-apt-repository ppa:savoury1/build-tools -y
+        sudo apt-get -y install autoconf2.71
+    fi
+fi
+
+if ! command -v yacc 2>&1 >/dev/null ; then
+    if command -v brew 2>&1 >/dev/null ; then
+        brew install byacc
+    elif command -v apt-get 2>&1 >/dev/null ; then
+        sudo apt-get update
+        sudo apt-get install -y byacc
+    else
+        echo "please install yacc"
     fi
 fi
 
@@ -108,16 +136,124 @@ else
     popd &> /dev/null
 fi
 
-if [ ! -f libnfs/local-install/lib/libnfs.a ]; then
-    pushd libnfs &> /dev/null
-    CURDIR="$(pwd)"
-    INSTALL_DIR="${CURDIR}/local-install"
-    mkdir -p "${INSTALL_DIR}"
-    ./bootstrap
-    ./configure --without-libkrb5 --prefix="${INSTALL_DIR}" --exec-prefix="${INSTALL_DIR}" CFLAGS='-fPIC -Wno-cast-align'
-    make
-    make install
-    popd &> /dev/null
+PROCS=8
+YACC="yacc"
+if [ "${OS}" == "Darwin" ]; then
+    YACC="/opt/homebrew/bin/byacc"
+fi
+if [ ! -f libnfs/local-install/${TARGET_TRIPLE}/lib/libnfs.a ]; then
+    if [ "${OS}" == "Darwin" ]; then
+        pushd libnfs &> /dev/null
+        CURDIR="$(pwd)"
+        INSTALL_DIR="${CURDIR}/local-install/${TARGET_TRIPLE}"
+        BUILD_DIR="${CURDIR}/local-build/${TARGET_TRIPLE}"
+        mkdir -p "${BUILD_DIR}"
+        mkdir -p "${INSTALL_DIR}"
+        chmod 775 ./bootstrap
+        ./bootstrap
+        pushd ${BUILD_DIR} &> /dev/null
+        ../../configure \
+            --disable-werror \
+            --host=${TARGET_TRIPLE_FOR_CC} \
+            --prefix="${INSTALL_DIR}" \
+            --exec-prefix="${INSTALL_DIR}" \
+            CFLAGS="-fPIC -Wno-cast-align -I${CURDIR}" \
+            YACC="${YACC}"
+        make -j${PROCS} install
+        popd &> /dev/null
+        popd &> /dev/null
+    elif [ "${OS}" == "Linux" ]; then
+        MAIN_CURDIR="$(pwd)"
+
+        EXTRA_CFLAGS=""
+        EXTRA_LDFLAGS=""
+
+        HOST_ARCH=`uname -m`
+        COMPILE_FOR_ARCH=`echo ${TARGET_TRIPLE} | awk -F '-' '{print $1}'`
+        CROSS_COMPILE="false"
+        if [ "${HOST_ARCH}" != "${COMPILE_FOR_ARCH}" ]; then
+            CROSS_COMPILE="true"
+        fi
+
+        if [ "${CROSS_COMPILE}" == "true" ]; then
+            OPENSSL_INSTALL_DIR="${MAIN_CURDIR}/openssl/local-install/${TARGET_TRIPLE}"
+            EXTRA_CFLAGS="-I${OPENSSL_INSTALL_DIR}/include"
+            EXTRA_LDFLAGS="-L${OPENSSL_INSTALL_DIR}/lib"
+            if [ ! -e openssl ]; then
+                mkdir -p ${OPENSSL_INSTALL_DIR}
+                echo "building openssl for cross compile"
+                if [ "${HOST_ARCH}" == "x86_64" ]; then
+                    sudo add-apt-repository -s "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main restricted universe multiverse" -y
+                else
+                    sudo add-apt-repository -s "deb http://ports.ubuntu.com/ubuntu-ports $(lsb_release -sc) main restricted universe multiverse" -y
+                fi
+                apt-get source openssl
+                OPENSSL_VER=`apt-cache showsrc openssl | grep '^Version' | awk '{print $2}' | awk -F '-' '{print $1}'`
+                OPENSSL_SRC_DIR="openssl-${OPENSSL_VER}"
+                pushd ${OPENSSL_SRC_DIR}
+                ./Configure linux-${COMPILE_FOR_ARCH} --prefix=${OPENSSL_INSTALL_DIR} CC=${COMPILE_FOR_ARCH}-linux-gnu-gcc
+                make -j${PROCS}
+                make -j${PROCS} install
+                popd
+            fi
+        fi
+
+        if [ ! -e krb5 ]; then
+            git clone --branch krb5-1.21.3-final https://github.com/krb5/krb5.git
+        fi
+
+        KRB5_INSTALL_DIR="${MAIN_CURDIR}/krb5/local-install/${TARGET_TRIPLE}"
+
+        if [ ! -e krb5/local-install/${TARGET_TRIPLE}/lib/krb5 ]; then
+            pushd krb5 &> /dev/null
+            CURDIR="$(pwd)"
+            INSTALL_DIR="${CURDIR}/local-install/${TARGET_TRIPLE}"
+            mkdir -p "${INSTALL_DIR}"
+            BUILD_DIR="${CURDIR}/local-build/${TARGET_TRIPLE}"
+            mkdir -p "${BUILD_DIR}"
+            pushd src &> /dev/null
+            # for cross compile to work
+            export krb5_cv_attr_constructor_destructor=yes
+            export ac_cv_func_regcomp=yes
+            export ac_cv_printf_positional=yes
+            autoreconf --force
+            pushd ${BUILD_DIR} &> /dev/null
+            ../../src/configure \
+                --host=${TARGET_TRIPLE_FOR_CC} \
+                --prefix="${INSTALL_DIR}" \
+                --exec-prefix="${INSTALL_DIR}" \
+                --enable-static \
+                --disable-shared \
+                CFLAGS="-fPIC -fcommon -Wno-cast-align ${EXTRA_CFLAGS}" \
+                LDFLAGS="${EXTRA_LDFLAGS}"
+            make -j${PROCS}
+            make install
+            popd &> /dev/null
+            popd &> /dev/null
+            popd &> /dev/null
+        fi
+
+        pushd libnfs &> /dev/null
+        CURDIR="$(pwd)"
+        INSTALL_DIR="${CURDIR}/local-install/${TARGET_TRIPLE}"
+        BUILD_DIR="${CURDIR}/local-build/${TARGET_TRIPLE}"
+        mkdir -p "${BUILD_DIR}"
+        mkdir -p "${INSTALL_DIR}"
+        chmod 775 ./bootstrap
+        ./bootstrap
+        pushd ${BUILD_DIR} &> /dev/null
+        ../../configure \
+            --disable-werror \
+            --host=${TARGET_TRIPLE_FOR_CC} \
+            --prefix="${INSTALL_DIR}" \
+            --exec-prefix="${INSTALL_DIR}" \
+            CFLAGS="-fPIC -Wno-cast-align -I${KRB5_INSTALL_DIR}/include -I${CURDIR}" \
+            LDFLAGS="-L${KRB5_INSTALL_DIR}/lib" \
+            LIBS="-ldl -lgssapi_krb5 -lkrb5 -lcom_err -lgssrpc -lk5crypto -lkdb5 -lkrad -lkrb5_db2 -lkrb5_k5tls -lkrb5_otp -lkrb5_spake -lkrb5support -lverto -lresolv"
+        make -j${PROCS} install
+        popd &> /dev/null
+        popd &> /dev/null
+    fi
 fi
 
 if [ ! -d go-nfs ]; then
